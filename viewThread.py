190e2eb7714dca8f503b37870626a2b45662d20a
kro0ol <clics_98@mail.ru>
2023-10-08 18:28:26 +0700

    viewThread v.2

--- viewThread.py
+++ viewThread.py
@@ -1,24 +1,38 @@
 import json
+import os
 
-with open('data.txt', 'r') as file:
-    data = file.read()
+def checkBlock(path):
+    with open(path, 'r') as file:
+        data = file.read()
 
-blocks = data.split('}{')  # разбиваем данные на блоки
+    blocks = data.split('}{')  # разбиваем данные на блоки
 
-for block in blocks:
-    if block[0] != '{':  # если блок начинается не с '{', то добавляем его
-        block = '{' + block
-    if block[-1] != '}':  # если блок заканчивается не '}', то добавляем его
-        block += '}'
+    for block in blocks:
+        if block[0] != '{':  # если блок начинается не с '{', то добавляем его
+            block = '{' + block
+        if block[-1] != '}':  # если блок заканчивается не '}', то добавляем его
+            block += '}'
 
-    block_data = json.loads(block)  # преобразуем блок в словарь
+        block_data = json.loads(block)  # преобразуем блок в словарь
 
-    print('Block index:', block_data['index'])
-    print('Timestamp:', block_data['timestamp'])
-    print('Transactions:')
-    for tx in block_data['transaction']:
-        print('  ', tx['sender'], '->', tx['receiver'], '(', tx['amount'], ')')
-    print('Previous hash:', block_data['previous_hash'])
-    print('Hash:', block_data['hash'])
-    print('Nonce:', block_data['nonce'])
-    print()
+        print('Block index:', block_data['index'])
+        print('Timestamp:', block_data['timestamp'])
+        print('Transactions:')
+        for tx in block_data['transaction']:
+            print('  ', tx['sender'], '->', tx['receiver'], '(', tx['amount'], ')')
+        print('Previous hash:', block_data['previous_hash'])
+        print('Hash:', block_data['hash'])
+        print('Nonce:', block_data['nonce'])
+        print()
+
+
+while True:
+    path = input('Enter path to chain: ')
+    if os.path.isfile(path):
+        try:
+            checkBlock(path)
+        except ValueError:
+            print('Error! Invalid data in file.')
+    else:
+        print('Error! File not found at the specified path.')
+    break
